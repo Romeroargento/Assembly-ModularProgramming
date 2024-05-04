309f6b83dd611989804a471c9c7a62065e2a0ab0
Romeroargento <105880916+Romeroargento@users.noreply.github.com>
2024-05-04 19:38:58 +0300

    Add files via upload

---
+++ cpponly/lab4cpp/lab4cpp.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+#include <chrono>
+using namespace std;
+
+int main() {
+    int i, k;
+    int n = 1000000;
+    float x;
+    float y;
+    float f = 0;
+    cout << "n = " << n << endl;
+
+    cout << "Input x: ";
+    cin >> x;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    for (i = 0; i < n; i++) {
+        f = fabsf(1 / tanf(x) - sinf(x));
+    }
+
+    cout << "result: " << f << endl;
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "Execution Time: " << duration.count() << " seconds" << endl;
+
+    return 0;
+}
