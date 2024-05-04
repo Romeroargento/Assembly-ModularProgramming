51a4b89907bc7e535039328fb77de927a5f070ae
Romeroargento <105880916+Romeroargento@users.noreply.github.com>
2024-05-04 19:56:13 +0300

    Update Source.asm

--- lab4/Source.asm
+++ lab4/Source.asm
@@ -6,7 +6,7 @@
 result DD 10 dup (0.0)
 
 .CODE 
-extern fun_el:near ; îáúÿâëåíèå âíåøíåé ôóíêöèè fun_el 
+extern fun_el:near ; объявление внешней функции fun_el 
 
 public FuncValue 
 FuncValue proc C n:DWORD, x:DWORD
@@ -14,25 +14,25 @@ FuncValue proc C n:DWORD, x:DWORD
 	xor ebx, ebx
 
 loop_start:
-	cmp ebx, ecx; ñðàâíåíèå èòåðàöèè è ðàçìåðà ìàññèâà
+	cmp ebx, ecx; сравнение итерации и размера массива
 	jge endo
 
-	mov edx, dword ptr [eax] ; ïåðåäà÷à ïåðâîãî ýëåìåíòà ìàññèâà â edx
-	push ecx ; ñîõðàíåíèå ðàçìåðà ìàññèâà
-	push eax ; ñîõðàíåíèå ìàññèâà
-	push edx ; ïåðåäà÷à ýëåìåíòà ìàññèâà â ôóíêöèþ fun_el
+	mov edx, dword ptr [eax] ; передача первого элемента массива в edx
+	push ecx ; сохранение размера массива
+	push eax ; сохранение массива
+	push edx ; передача элемента массива в функцию fun_el
 	call fun_el
 	pop edx
 	pop eax
 	pop ecx
-	add eax, 4 ; ïåðåõîä íà ñëåäóþùèé ýëåìåíò ìàññèâà 
-	fstp result[ebx*4] ; ïåðåäà÷à îòâåòà â ìàññèâ result
-	inc ebx; óâåëè÷åíèå èòåðàöèè 
+	add eax, 4 ; переход на следующий элемент массива 
+	fstp result[ebx*4] ; передача ответа в массив result
+	inc ebx; увеличение итерации 
 
 	jmp loop_start
 
 endo:
-	mov eax, offset result ; ïåðåäà÷à àäðåñà ìàññèâà result â eax
+	mov eax, offset result ; передача адреса массива result в eax
 	ret
 	FuncValue endp 
 End 
